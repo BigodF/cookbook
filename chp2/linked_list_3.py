6317327a3fd3ba17fb904b184371b4ba4b9dadf1
heran5 <heran5@staff.weibo.com>
2026-03-10 20:04:10 +0800

    -

---
+++ chp2/linked_list_3.py
@@ -0,0 +1,46 @@
+from typing import List, Optional
+from functools import cmp_to_key
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+def list2linked_list(nums):
+    head = tmp = None
+    for t in nums:
+        if tmp == None:
+            tmp = ListNode(t)
+            head = tmp
+        else:
+            tmp.next = ListNode(t)
+            tmp = tmp.next
+    return head
+
+def linked_list2list(head):
+    res = []
+    while head:
+        res.append(head.val)
+        head = head.next
+    return res
+
+class Solution:
+    def test(self, ):
+        head = [1,1,2]
+        res = [1,2]
+        
+        head = [1,1,2,3,3]
+        res = [1, 2, 3]
+        
+        
+        head = list2linked_list(head)
+        # res = list2linked_list(res)
+        
+        r = self.hasCycle(head)
+        
+        r = linked_list2list(r)
+        print(r)
+        print(r==res)
+        return
+        
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
